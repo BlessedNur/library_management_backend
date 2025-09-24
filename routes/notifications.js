67427306ab781b6d941cf110b1b8d72223b972f2
BlessedNur <blessednur67@gmail.com>
2025-09-24 17:13:54 +0100

    done

--- routes/notifications.js
+++ routes/notifications.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const notificationController = require("../controllers/notificationController");
-const auth = require("../middleware/auth");
+const { protect, adminOnly } = require("../middleware/auth");
 
 // Test email system
 router.get("/test", notificationController.testEmailSystem);
@@ -9,14 +9,16 @@ router.get("/test", notificationController.testEmailSystem);
 // Send return reminders
 router.post(
   "/return-reminders",
-  auth,
+  protect,
+  adminOnly,
   notificationController.sendReturnReminders
 );
 
 // Send overdue notifications
 router.post(
   "/overdue-notifications",
-  auth,
+  protect,
+  adminOnly,
   notificationController.sendOverdueNotifications
 );
 
