c6904f5c923bb5cbc2fa4ab883f7dd7ace1f5c14
BlessedNur <blessednur67@gmail.com>
2025-09-24 17:05:26 +0100

    done

--- services/emailService.js
+++ services/emailService.js
@@ -1,10 +1,10 @@
 const nodemailer = require("nodemailer");
 
-const transporter = nodemailer.createTransporter({
-  service: "gmail", 
+const transporter = nodemailer.createTransport({
+  service: "gmail",
   auth: {
-    user: process.env.EMAIL_USER, 
-    pass: process.env.EMAIL_PASS, 
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
   },
 });
 
